6a41a0cf149abfe9b48e120c72464eaa49afd7e0
migmm <micmigm@gmail.com>
2023-06-07 19:50:40 -0300

    fix origin

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -2,7 +2,8 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-    origin:"http://localhost:5000",
+    origin:process.env.ORIGIN1,
+    origin2:process.env.ORIGIN2,
     port: process.env.MONGODB_PORT,
     mongoURI: process.env.MONGODB_URI,
     googleClientId: process.env.CLIENT_ID,
